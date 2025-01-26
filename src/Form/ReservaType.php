297645d12a5ae7fb386de900c7e3e3f8b705e02e
Cidraque <juanfransc@hotmail.com>
2025-01-26 13:12:10 +0000

    creo crud de ambas entidades

---
+++ src/Form/ReservaType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Reserva;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ReservaType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('comensales')
+            ->add('fecha', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('usuario', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Reserva::class,
+        ]);
+    }
+}
